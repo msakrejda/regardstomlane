e6d8fe2b56c923fb2ea6e6587b225db1abcc2e57
Maciek Sakrejda <maciek@heroku.com>
2017-03-14 10:34:19 -0700

    Tweak tests to assume proper capitalization

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,19 +5,19 @@ require_relative '../helpers'
 
 describe "@regardstomlane" do
   tests = [
-    [ "hello. regards, tom lane", "hello." ],
-    [ "hello? regards, tom lane", "hello?" ],
-    [ "hello! regards, tom lane", "hello!" ],
-    [ "hello world. regards, tom lane", "hello world." ],
-    [ "hello\nworld. regards, tom lane", "hello world." ],
-    [ "hello world. goodbye. regards, tom lane", "goodbye." ],
-    [ "hello world. goodbye.  regards, tom lane", "goodbye." ],
-    [ "hello world.\ngoodbye. regards, tom lane", "goodbye." ],
-    [ "hello world. goodbye. \n regards, tom lane", "goodbye." ],
-    [ "hello world. c.c.h. pounder is an actor. regards, tom lane",
-      "c.c.h. pounder is an actor." ],
-    [ "hello world. c.c.h. pounder is an actor. b.d. wong is also an actor. regards, tom lane",
-      "b.d. wong is also an actor." ],
+    [ "Hello. regards, tom lane", "Hello." ],
+    [ "Hello? regards, tom lane", "Hello?" ],
+    [ "Hello! regards, tom lane", "Hello!" ],
+    [ "Hello world. regards, tom lane", "Hello world." ],
+    [ "Hello\nworld. regards, tom lane", "Hello world." ],
+    [ "Hello world. goodbye. regards, tom lane", "goodbye." ],
+    [ "Hello world. goodbye.  regards, tom lane", "goodbye." ],
+    [ "Hello world.\ngoodbye. regards, tom lane", "goodbye." ],
+    [ "Hello world. goodbye. \n regards, tom lane", "goodbye." ],
+    [ "Hello world. C.C.H. Pounder is an actor. regards, tom lane",
+      "C.C.H. Pounder is an actor." ],
+    [ "Hello world. C.C.H. pounder is an actor. B.D. Wong is also an actor. regards, tom lane",
+      "B.D. Wong is also an actor." ],
     [ "Something something. There's definitely a CommandCounterIncrement in exec_execute_message ...\nregards, tom lane",
       "There's definitely a CommandCounterIncrement in exec_execute_message ..."]
   ]
