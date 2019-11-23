610d497fcb36ad6bd8074ad36a35d06ed67045f3
Maciek Sakrejda <m.sakrejda@gmail.com>
2019-11-22 16:43:22 -0800

    Rewrite last-sentence-matching algorithm

--- helpers.rb
+++ helpers.rb
@@ -1,5 +1,28 @@
 def find_last_sentence(email_body)
-  if email_body =~ /(?:.*?[.!?]\s+)?((?:\w\.)*[^.]*(?:[.!?]| \.{3}))\s+regards, tom lane/im
-    $1.gsub("\n", " ")
+  # 1. Match every line before "regards, tom lane" going back
+  #    to just after after the last >quoted line (if any). We
+  #    assume the last line is not going to span a quote and
+  #    ignore any that do.
+  unless email_body =~ /((?:^[^>].*?\n)+)\s*regards, tom lane/im
+    return
   end
+
+  # 2. Tokenize on whitespace
+  parts = $1.scan(/\s+|\S+/)
+
+  # 3. Go backwards through tokens starting at right before signature
+  previous = nil
+  parts.reverse.take_while do |token|
+    # 4. Stop at token that ends the previous sentence:
+    #    a. is the following (i.e., previous, since we're going backwards) word capitalized?
+    #    b. if so, does this word end in a period, question mark or exclamation mark and
+    #       start with a lowercase letter?
+    match = previous.nil? || previous !~ /^[A-Z]/ || token !~ /[a-z]+(?:[.?!]| ...)/
+    if token =~ /\S+/
+      previous = token
+    end
+    match
+  end.reverse.map do |token|
+    token == "\n" ? " " : token
+  end.join.strip
 end
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,22 +5,27 @@ require_relative '../helpers'
 
 describe "@regardstomlane" do
   tests = [
-    [ "Hello. regards, tom lane", "Hello." ],
-    [ "Hello? regards, tom lane", "Hello?" ],
-    [ "Hello! regards, tom lane", "Hello!" ],
-    [ "Hello world. regards, tom lane", "Hello world." ],
-    [ "Hello\nworld. regards, tom lane", "Hello world." ],
-    [ "Hello world. goodbye. regards, tom lane", "goodbye." ],
-    [ "Hello world. goodbye.  regards, tom lane", "goodbye." ],
-    [ "Hello world.\ngoodbye. regards, tom lane", "goodbye." ],
-    [ "Hello world. goodbye. \n regards, tom lane", "goodbye." ],
-    [ "Hello world. C.C.H. Pounder is an actor. regards, tom lane",
+    [ "Hello.\nregards, tom lane", "Hello." ],
+    [ "Hello?\nregards, tom lane", "Hello?" ],
+    [ "Hello!\nregards, tom lane", "Hello!" ],
+    [ "Hello world.\nregards, tom lane", "Hello world." ],
+    [ "Hello\nworld.\nregards, tom lane", "Hello world." ],
+    [ "Hello world. Goodbye.\nregards, tom lane", "Goodbye." ],
+    [ "Hello world.\nGoodbye.\nregards, tom lane", "Goodbye." ],
+    [ "Hello world. C.C.H. Pounder is an actor.\nregards, tom lane",
       "C.C.H. Pounder is an actor." ],
-    [ "Hello world. C.C.H. pounder is an actor. B.D. Wong is also an actor. regards, tom lane",
+    [ "Hello world. C.C.H. Pounder is an actor. B.D. Wong is also an actor.\nregards, tom lane",
       "B.D. Wong is also an actor." ],
     [ "Something something. There's definitely a CommandCounterIncrement in exec_execute_message ...\nregards, tom lane",
-      "There's definitely a CommandCounterIncrement in exec_execute_message ..."]
-  ]
+      "There's definitely a CommandCounterIncrement in exec_execute_message ..."],
+    [ %q(Which is why we invented the ENCRYPTED PASSWORD syntax, as well as
+psql's \password command ... but using that approach for actual
+login to an account would be a security fail as well.
+
+
+
+			regards, tom lane), "Which is why we invented the ENCRYPTED PASSWORD syntax, as well as psql's \\password command ... but using that approach for actual login to an account would be a security fail as well." ],
+    ]
 
   tests.each do |(body, expected)|
     it "extracts '#{expected}' from '#{body}'" do
